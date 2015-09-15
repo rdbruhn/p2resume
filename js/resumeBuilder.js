76ab8132ab06c006289232c8d3d5fdf8ae64e2a1
rdbruhn <rdbruhn@gmail.com>
2015-09-15 00:47:14 -0400

    Initial Commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,333 @@
+var bio = {
+    "name": "Rob Bruhn",
+    "role": "Front-End Web Developer & Student Ministry Specialist",
+    "welcome": "I'm seeking to finish my Udacity nanodegree, complete freelance projects, and continue training as a front-end web developer. I currently work with high school and middle school students in spiritual growth and leadership development. Thank you for checking out my resume! I'm always looking for new challenges.",
+    "contacts": {
+        "mobile": "610.314.5306",
+        "email": "rdbruhn@gmail.com",
+        "github": "rdbruhn",
+        "twitter": "rdbforgiven",
+        "location": "West Chester, PA"
+    },
+    "skills": ["HTML", "CSS", "JavaScript", "Microsoft Office", "iWork", "intermediate Chinese", "Guitar", "Teaching", "Writing"],
+    "biopic": "images/rob.jpg"
+}
+
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Goshen Baptist Church",
+            "url": "http://www.goshenbaptist.org",
+            "title": "Director of Student Ministries",
+            "location": "West Chester, PA",
+            "dates": "April 2014-present",
+            "url": "http://www.goshenbaptist.org",
+            "description": "Led middle and high school student service, through weekly meetings, small groups, and large group events. -Maintained regular email, website, phone, and interpersonal communication with parents and students concerning events, teaching, and partnership opportunities. -Mobilized and led a team of volunteers, including leadership training, bible study and prayer. -Planned, implemented, and managed a full youth ministry calendar and a $9,000 budget. -Networked with area youth pastors and church leaders to develop cross-congregational partnerships."
+        },
+       {
+            "employer": "Pioneers",
+            "title": "Team Member",
+            "location": "East Asia",
+            "dates": "January 2009-December 2013",
+            "url": "http://www.pioneers.org",
+            "description": "Served in Cross-Cultural relationship-building with minority university students in China. -Studied local languages and cultures in order to more effectively communicate. -Created a digital library of key cultural and religious resources for workers in East Asia "
+       },
+       {
+            "employer": "Recreational Equipment Inc.",
+            "title": "Customer Service Specialist",
+            "location": "Oak Brook, IL",
+            "dates": "May 2011-January 2012",
+            "url": "http://www.rei.com",
+            "description": "Provided comprehensive customer service and product knowledge for an outdoor sports retailer. -Efficiently processed purchases, returns, special orders, and various services both in person and over the phone."
+       },
+       {
+            "employer": "Recreational Equipment Inc.",
+            "title": "Customer Service Specialist",
+            "location": "Fairfax, VA",
+            "dates": "November 2010-May 2011",
+            "url": "http://www.rei.com",
+            "description": "Provided comprehensive customer service and product knowledge for an outdoor sports retailer. -Efficiently processed purchases, returns, special orders, and various services both in person and over the phone."
+       },
+       {
+            "employer": "College Church in Wheaton",
+            "title": "Youth Ministry Associate",
+            "location": "Wheaton, IL",
+            "dates": "August 2008-June 2009",
+            "url": "http://www.college-church.org",
+            "description": "Taught and discipled high school students one-on-one and in small groups. -Led high school music team in study and leadership of worship. -Worked with team to develop, organize, and run multiple gospel-centered youth ministry events and activities. -Preached and taught the Bible in large and small group settings."
+       },
+       {
+            "employer": "Wheaton College",
+            "title": "Resident Assistant",
+            "location": "Wheaton, IL",
+            "dates": "August 2005-May 2006",
+            "url": "http://www.wheaton.edu",
+            "description": "Managed logistical aspects of the dormitory such as check-in, check-out, and discipline. -Promoted community, fellowship, and spiritual development of students living in the dorm through programming and intentional relationships. -Cooperated with other ResLife staff to create a positive dorm environment and community"
+       }
+    ]
+}
+
+//TODO: replace placeholder images with project images
+
+var projects =  {
+    "projects": [
+        {
+            "title": "Online Portfolio Site",
+            "dates": "April 2015",
+            "description": "Built a site to display an online portfolio of projects using html, css, and the boostrap framework. Project also inculdes the use of carousel, popup descriptions, and a contact button.",
+            "url": "https://github.com/rdbruhn/p1portfolio",
+            "images": [
+                "images/portfolio.jpg",
+                "http://placehold.it/100x100",
+                "http://placehold.it/100x100",
+                "http://placehold.it/100x100"
+            ]
+        },
+        {
+            "title": "Online Resume",
+            "dates": "June-July 2015",
+            "description": "Built a site to display an online resume using html, javaScript, and jquery. Project includes heavy use of the replace function, hide and show functions and personalized css.",
+            "url": "https://github.com/rdbruhn/frontend-nanodegree-resume",
+            "images": [
+                "images/resume.jpg",
+                "http://placehold.it/100x100",
+                "http://placehold.it/100x100",
+                "http://placehold.it/100x100"
+            ]
+        }
+    ]
+}
+
+
+var education = {
+    "schools": [
+        {
+            "name": "Udacity",
+            "location": "Mountain View, CA",
+            "degree": "Front-End Web Developer Nanodegree",
+            "majors": [
+                "Front-End Web Developer"
+            ],
+            "dates": "April 2015-present",
+            "url" : "http://www.udacity.com"
+        },
+        {
+            "name": "Wheaton College",
+            "location": "Wheaton, IL",
+            "degree": "M.A.",
+            "majors": [
+                "Biblical Exegesis"
+            ],
+            "dates": "2007-2009",
+            "url" : "http://www.wheaton.edu"
+        },
+        {
+            "name": "Wheaton College",
+            "location": "Wheaton, IL",
+            "degree": "B.A.",
+            "majors": [
+                "Christian Education",
+                "Youth Ministry Certificate"
+            ],
+            "dates": "2003-2006",
+            "url" : "http://www.wheaton.edu"
+        }
+    ],
+    "onlineClasses": [
+        {
+            "title": "Intro to HTML and CSS",
+            "school": "Udacity",
+            "dates": "April 2015", 
+            "url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+        },
+        {
+            "title": "Responsive Web Design Fundamentals",
+            "school": "Udacity",
+            "dates": "April 2015", 
+            "url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"
+        },
+        {
+            "title": "Responsive Images",
+            "school": "Udacity",
+            "dates": "April 2015", 
+            "url": "https://www.udacity.com/course/responsive-images--ud882"
+        },
+        {
+            "title": "How to Use Git and GitHub",
+            "school": "Udacity",
+            "dates": "May 2015", 
+            "url": "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+        },
+        {
+            "title": "JavaScript Basics",
+            "school": "Udacity",
+            "dates": "July-August 2015", 
+            "url": "https://www.udacity.com/course/javascript-basics--ud804"
+        },
+        {
+            "title": "Intro to jquery",
+            "school": "Udacity",
+            "dates": "August-September 2015", 
+            "url": "https://www.udacity.com/course/intro-to-jquery--ud245"
+        }
+    ]
+}
+
+//function replaces bio information when available
+
+bio.display = function() {
+    if(bio.role.length > 0) {
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $("#header").prepend(formattedRole);
+    }
+    if(bio.name.length > 0) {
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(formattedName); 
+    }
+    if (bio.contacts.mobile.length > 0) {
+        var formattedMobile = HTMLmobile.replace(/%data%/g, bio.contacts.mobile);
+        $("#topContacts").append(formattedMobile);
+        $("#footerContacts").append(formattedMobile);
+    }
+    if (bio.contacts.email.length > 0) {
+        var formattedEmail = HTMLemail.replace(/%data%/g, bio.contacts.email);
+        $("#topContacts").append(formattedEmail);
+        $("#footerContacts").append(formattedEmail); 
+    }
+    if (bio.contacts.github.length > 0) {
+        var formattedGithub = HTMLgithub.replace(/%data%/g, bio.contacts.github);
+        $("#topContacts").append(formattedGithub);
+        $("#footerContacts").append(formattedGithub); 
+    }
+    if (bio.contacts.twitter.length > 0) {
+        var formattedTwitter = HTMLtwitter.replace(/%data%/g, bio.contacts.twitter);
+        $("#topContacts").append(formattedTwitter); 
+        $("#footerContacts").append(formattedTwitter); 
+    }
+    if (bio.contacts.location.length > 0) {
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        $("#topContacts").append(formattedLocation);
+        $("#footerContacts").append(formattedLocation); 
+    }
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        $("#header").append(formattedBioPic);
+    
+    if(bio.welcome.length > 0) {
+        var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+        $("#header").append(formattedWelcome); 
+    }
+    if(bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+    }
+    for(skill in bio.skills) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+        $("#skills").append(formattedSkill);
+    }
+}
+
+bio.display();
+
+//function replaces work information when available
+
+work.display = function() {
+    for(job in work.jobs) {
+        $("#subWorkExp").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        formattedEmployer = formattedEmployer.replace("%url%", work.jobs[job].url);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle + formattedWorkLocation + formattedWorkDates  + formattedDescription);
+    }
+}
+
+
+work.display();
+
+//function replaces projects information when available
+
+projects.display = function() {
+    for (project in projects.projects) {
+        $("#subProjects").append(HTMLprojectStart);
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        formattedProjectTitle = formattedProjectTitle.replace("%url%", projects.projects[project].url);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedProjectDescriptrion = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        var formattedProject = formattedProjectTitle + formattedProjectDates + formattedProjectDescriptrion;
+        $(".project-entry:last").append(formattedProject);
+        var len = projects.projects[project].images.length;
+        for (var image = 0; image < len; image++) {
+                var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedProjectImage);
+            }
+    }
+}
+
+
+projects.display();
+
+//function replaces education information when available
+
+education.display = function() {
+    for (school in education.schools) {
+        $("#subEducation").append(HTMLschoolStart);
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        formattedSchoolName = formattedSchoolName.replace("%url%", education.schools[school].url);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        var formattedSchool = formattedSchoolName  + formattedSchoolDegree + formattedSchoolLocation + formattedSchoolDates;
+        $(".education-entry:last").append(formattedSchool);
+        var len = education.schools[school].majors.length;
+        for (var major = 0; major < len; major++) {
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]);
+            $(".education-entry:last").append(formattedSchoolMajor);
+            }
+    } 
+    $(".education-entry:last").append("<hr>"); 
+    $(".education-entry:last").append(HTMLonlineClasses); 
+    var len = education.onlineClasses.length;
+    for (var classes = 0; classes < len; classes++){   
+            var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineClasses[classes].title);
+            formattedOnlineTitle = formattedOnlineTitle.replace("%url%", education.onlineClasses[classes].url);
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineClasses[classes].school);
+            var formattedOnlineTitle = formattedOnlineTitle + formattedOnlineSchool;
+            $(".education-entry:last").append(formattedOnlineTitle);
+            var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineClasses[classes].dates);
+            $(".education-entry:last").append(formattedOnlineDates);
+            var formattedOnlineUrl = HTMLonlineURL.replace("#", education.onlineClasses[classes].url);
+            $(".education-entry:last").append("<br>");
+    } 
+}
+
+
+education.display();
+
+//appends google map from helper.js to subMap
+
+$("#subMap").append(googleMap);
+
+//function hides and shows each sub-section make the resume eaiser to navigate
+
+function hideshow(which){
+if (!document.getElementById)
+return
+$(which).toggle();
+}
+
+//function changes Name at tope of page to international format
+
+function inName() {
+    var newName = bio.name.split(" ");
+    newName[1] = newName[1].toUpperCase();
+    console.log(newName[1]);
+    newName[0] = newName[0].slice(0,1).toUpperCase() + newName[0].slice(1).toLowerCase();
+    
+    return newName[0] + " " + newName [1];
+}
+
+
+//$("#main").append(internationalizeButton);
